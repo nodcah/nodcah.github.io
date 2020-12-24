01a69c2106d4dd215c0dbfcd0a9f110ba0f16708
nodcah <nodcah@gmail.com>
2020-12-24 15:49:40 -0500

    Initial push of new content

--- javascript.js
+++ javascript.js
@@ -1,53 +1,8 @@
-// JavaScript Document
+// JavaScript inits and overrides
+(function ($) {
+  $(function () {
 
-//Google Analytics
-$(document).ready(function () {
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+    $('.sidenav').sidenav();
 
-    ga('create', 'UA-56969581-1', 'auto');
-    ga('require', 'displayfeatures');
-    ga('send', 'pageview');
-
-
-    //dropdown
-    $(".dropdown-button").dropdown({
-        hover: true,
-        belowOrigin: true
-    });
-
-    //mobileSidebar
-    $('.button-collapse').sideNav({
-        'edge': 'left',
-        menuWidth: '275'
-
-    });
-    //collapsibleSidebar
-    $('.collapsible').collapsible();
-});
-
-//sticky nav bar
-window.onscroll = changePos;
-
-function changePos() {
-    //document.getElementById("foo").innerHTML = document.getElementById("foo").innerHTML + 1;
-    var navbar = document.getElementById("navbar, mobileNav");
-    if (window.pageYOffset > 103 && window.innerWidth > 992) {
-        navbar.style.top = (window.pageYOffset - 103) + "px";
-        //  } else if (window.innerWidth <= 992) {
-        //     navbar.style.top = navbar.pageYOffset + "px";
-        //  }
-        else {
-            navbar.style.top = navbar.pageYOffset + "px";
-            // navbar.style.top = "0px";
-        }
-    }
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
