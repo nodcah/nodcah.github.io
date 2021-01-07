f51e981704feefa5f5bf6ea41a7c7ffb5e15ccb1
nodcah <nodcah@gmail.com>
2021-01-07 03:18:30 -0500

    Gettin there

--- javascript.js
+++ javascript.js
@@ -1,9 +1,35 @@
 // JavaScript inits and overrides
+
 (function ($) {
   $(function () {
-
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+    $('#nav-mobile').on('click', function () {
+      $$.sidenav.sidenav('close');
+    });
+
+    $(window).scroll(checkScroll);
+    checkScroll();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+var $$ = { // Cache
+  window: $(window),
+  navName: $('#nav-name'),
+  sidenav: $('.sidenav')
+};
+var isAtTopOfPage = false;
+
+function checkScroll() {
+  //console.log($(window).scrollTop())
+  if (isAtTopOfPage && $$.window.scrollTop() > 170) { //$('#mainname').position().top+$('#mainname').height()) {
+    $$.navName.text('Noah Del Coro');
+    $$.navName.addClass('fadein');
+    isAtTopOfPage = false;
+  } else if (!isAtTopOfPage && $$.window.scrollTop() < 170) { //$('#mainname').position().top+$('#mainname').height()) {
+    $$.navName.text('');
+    $$.navName.removeClass('fadein');
+    isAtTopOfPage = true;
+  }
+}
